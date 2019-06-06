3d3562ac267979406afaca0d003d1445fe742504
Chris Naismith <christophernaismith@gmail.com>
2019-06-05 20:43:55 -0400

    make not equal query use ne

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -35,7 +35,7 @@ export const pageQuery = graphql`
     }
 
     NotEqualArticles: allStrapiArticle(
-      filter: { tags: { elemMatch: { tag: { nin: ["Featured"] } } } }
+      filter: { tags: { elemMatch: { tag: { ne: ["Featured"] } } } }
     ) {
       nodes {
         id
